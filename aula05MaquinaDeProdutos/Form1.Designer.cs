7da79931b9aa63fa94f945802cf7f8f1c10405ff
Leonardo Yosuke Konichi <135863260+LeoYosuke@users.noreply.github.com>
2023-07-18 14:05:27 -0300

    Add files via upload

---
+++ aula05MaquinaDeProdutos/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace aula05MaquinaDeProdutos
+{
+    partial class BTNcompra
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBemvindo = new System.Windows.Forms.Label();
+            this.BTNmarmita1 = new System.Windows.Forms.Button();
+            this.BTNmarmita2 = new System.Windows.Forms.Button();
+            this.BTNmarmita3 = new System.Windows.Forms.Button();
+            this.BTNmarmita4 = new System.Windows.Forms.Button();
+            this.BTNCancelar = new System.Windows.Forms.Button();
+            this.BTNcomprar = new System.Windows.Forms.Button();
+            this.LBLpreco = new System.Windows.Forms.Panel();
+            this.LBLTOTAL = new System.Windows.Forms.Label();
+            this.LBLpreco1 = new System.Windows.Forms.Label();
+            this.lblpreco3 = new System.Windows.Forms.Label();
+            this.LBLpreco2 = new System.Windows.Forms.Label();
+            this.lblpreco4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LBLtotalvalor = new System.Windows.Forms.Label();
+            this.txtdinheiro = new System.Windows.Forms.TextBox();
+            this.LBLpreco.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblBemvindo
+            // 
+            this.lblBemvindo.AutoSize = true;
+            this.lblBemvindo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBemvindo.Location = new System.Drawing.Point(105, 9);
+            this.lblBemvindo.Name = "lblBemvindo";
+            this.lblBemvindo.Size = new System.Drawing.Size(227, 25);
+            this.lblBemvindo.TabIndex = 0;
+            this.lblBemvindo.Text = "Ola Seja Bem-vindo ";
+            // 
+            // BTNmarmita1
+            // 
+            this.BTNmarmita1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNmarmita1.Location = new System.Drawing.Point(56, 40);
+            this.BTNmarmita1.Name = "BTNmarmita1";
+            this.BTNmarmita1.Size = new System.Drawing.Size(175, 68);
+            this.BTNmarmita1.TabIndex = 1;
+            this.BTNmarmita1.Text = "Arroz Feijão e Bisteca";
+            this.BTNmarmita1.UseVisualStyleBackColor = true;
+            this.BTNmarmita1.Click += new System.EventHandler(this.BTNmarmita1_Click);
+            // 
+            // BTNmarmita2
+            // 
+            this.BTNmarmita2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNmarmita2.Location = new System.Drawing.Point(282, 40);
+            this.BTNmarmita2.Name = "BTNmarmita2";
+            this.BTNmarmita2.Size = new System.Drawing.Size(168, 68);
+            this.BTNmarmita2.TabIndex = 2;
+            this.BTNmarmita2.Text = "Arroz Feijão e Panqueca";
+            this.BTNmarmita2.UseVisualStyleBackColor = true;
+            this.BTNmarmita2.Click += new System.EventHandler(this.BTNmarmita2_Click);
+            // 
+            // BTNmarmita3
+            // 
+            this.BTNmarmita3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNmarmita3.Location = new System.Drawing.Point(57, 121);
+            this.BTNmarmita3.Name = "BTNmarmita3";
+            this.BTNmarmita3.Size = new System.Drawing.Size(175, 60);
+            this.BTNmarmita3.TabIndex = 3;
+            this.BTNmarmita3.Text = "Arroz Feijão e Lasanha";
+            this.BTNmarmita3.UseVisualStyleBackColor = true;
+            this.BTNmarmita3.Click += new System.EventHandler(this.BTNmarmita3_Click);
+            // 
+            // BTNmarmita4
+            // 
+            this.BTNmarmita4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNmarmita4.Location = new System.Drawing.Point(282, 121);
+            this.BTNmarmita4.Name = "BTNmarmita4";
+            this.BTNmarmita4.Size = new System.Drawing.Size(168, 60);
+            this.BTNmarmita4.TabIndex = 4;
+            this.BTNmarmita4.Text = "Arroz Feijão e Peixe";
+            this.BTNmarmita4.UseVisualStyleBackColor = true;
+            this.BTNmarmita4.Click += new System.EventHandler(this.BTNmarmita4_Click);
+            // 
+            // BTNCancelar
+            // 
+            this.BTNCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNCancelar.Location = new System.Drawing.Point(324, 394);
+            this.BTNCancelar.Name = "BTNCancelar";
+            this.BTNCancelar.Size = new System.Drawing.Size(138, 36);
+            this.BTNCancelar.TabIndex = 5;
+            this.BTNCancelar.Text = "CANCELAR";
+            this.BTNCancelar.UseVisualStyleBackColor = true;
+            this.BTNCancelar.Click += new System.EventHandler(this.BTNCancelar_Click);
+            // 
+            // BTNcomprar
+            // 
+            this.BTNcomprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNcomprar.Location = new System.Drawing.Point(324, 336);
+            this.BTNcomprar.Name = "BTNcomprar";
+            this.BTNcomprar.Size = new System.Drawing.Size(138, 36);
+            this.BTNcomprar.TabIndex = 6;
+            this.BTNcomprar.Text = "COMPRAR";
+            this.BTNcomprar.UseVisualStyleBackColor = true;
+            this.BTNcomprar.Click += new System.EventHandler(this.BTNcomprar_Click);
+            // 
+            // LBLpreco
+            // 
+            this.LBLpreco.Controls.Add(this.lblpreco4);
+            this.LBLpreco.Controls.Add(this.LBLpreco2);
+            this.LBLpreco.Controls.Add(this.lblpreco3);
+            this.LBLpreco.Controls.Add(this.LBLpreco1);
+            this.LBLpreco.Controls.Add(this.BTNmarmita3);
+            this.LBLpreco.Controls.Add(this.BTNmarmita4);
+            this.LBLpreco.Controls.Add(this.BTNmarmita1);
+            this.LBLpreco.Controls.Add(this.BTNmarmita2);
+            this.LBLpreco.Location = new System.Drawing.Point(12, 26);
+            this.LBLpreco.Name = "LBLpreco";
+            this.LBLpreco.Size = new System.Drawing.Size(470, 233);
+            this.LBLpreco.TabIndex = 7;
+            // 
+            // LBLTOTAL
+            // 
+            this.LBLTOTAL.AutoSize = true;
+            this.LBLTOTAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLTOTAL.Location = new System.Drawing.Point(25, 352);
+            this.LBLTOTAL.Name = "LBLTOTAL";
+            this.LBLTOTAL.Size = new System.Drawing.Size(69, 20);
+            this.LBLTOTAL.TabIndex = 1;
+            this.LBLTOTAL.Text = "TOTAL:";
+            this.LBLTOTAL.Click += new System.EventHandler(this.LBLvalor_Click);
+            // 
+            // LBLpreco1
+            // 
+            this.LBLpreco1.AutoSize = true;
+            this.LBLpreco1.Location = new System.Drawing.Point(14, 67);
+            this.LBLpreco1.Name = "LBLpreco1";
+            this.LBLpreco1.Size = new System.Drawing.Size(36, 13);
+            this.LBLpreco1.TabIndex = 5;
+            this.LBLpreco1.Text = "R$ 16";
+            // 
+            // lblpreco3
+            // 
+            this.lblpreco3.AutoSize = true;
+            this.lblpreco3.Location = new System.Drawing.Point(17, 140);
+            this.lblpreco3.Name = "lblpreco3";
+            this.lblpreco3.Size = new System.Drawing.Size(36, 13);
+            this.lblpreco3.TabIndex = 6;
+            this.lblpreco3.Text = "R$ 15";
+            // 
+            // LBLpreco2
+            // 
+            this.LBLpreco2.AutoSize = true;
+            this.LBLpreco2.Location = new System.Drawing.Point(239, 66);
+            this.LBLpreco2.Name = "LBLpreco2";
+            this.LBLpreco2.Size = new System.Drawing.Size(36, 13);
+            this.LBLpreco2.TabIndex = 7;
+            this.LBLpreco2.Text = "R$ 18";
+            // 
+            // lblpreco4
+            // 
+            this.lblpreco4.AutoSize = true;
+            this.lblpreco4.Location = new System.Drawing.Point(242, 139);
+            this.lblpreco4.Name = "lblpreco4";
+            this.lblpreco4.Size = new System.Drawing.Size(36, 13);
+            this.lblpreco4.TabIndex = 8;
+            this.lblpreco4.Text = "R$ 20";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(294, 298);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 161);
+            this.panel1.TabIndex = 8;
+            // 
+            // LBLtotalvalor
+            // 
+            this.LBLtotalvalor.AutoSize = true;
+            this.LBLtotalvalor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLtotalvalor.Location = new System.Drawing.Point(90, 352);
+            this.LBLtotalvalor.Name = "LBLtotalvalor";
+            this.LBLtotalvalor.Size = new System.Drawing.Size(19, 20);
+            this.LBLtotalvalor.TabIndex = 9;
+            this.LBLtotalvalor.Text = "0";
+            this.LBLtotalvalor.Click += new System.EventHandler(this.LBLtotalvalor_Click);
+            // 
+            // txtdinheiro
+            // 
+            this.txtdinheiro.Location = new System.Drawing.Point(29, 375);
+            this.txtdinheiro.Name = "txtdinheiro";
+            this.txtdinheiro.Size = new System.Drawing.Size(100, 20);
+            this.txtdinheiro.TabIndex = 10;
+            this.txtdinheiro.TextChanged += new System.EventHandler(this.txtdinheiro_TextChanged);
+            // 
+            // BTNcompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 511);
+            this.Controls.Add(this.txtdinheiro);
+            this.Controls.Add(this.LBLtotalvalor);
+            this.Controls.Add(this.LBLTOTAL);
+            this.Controls.Add(this.BTNcomprar);
+            this.Controls.Add(this.BTNCancelar);
+            this.Controls.Add(this.lblBemvindo);
+            this.Controls.Add(this.LBLpreco);
+            this.Controls.Add(this.panel1);
+            this.Name = "BTNcompra";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.LBLpreco.ResumeLayout(false);
+            this.LBLpreco.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBemvindo;
+        private System.Windows.Forms.Button BTNmarmita1;
+        private System.Windows.Forms.Button BTNmarmita2;
+        private System.Windows.Forms.Button BTNmarmita3;
+        private System.Windows.Forms.Button BTNmarmita4;
+        private System.Windows.Forms.Button BTNCancelar;
+        private System.Windows.Forms.Button BTNcomprar;
+        private System.Windows.Forms.Panel LBLpreco;
+        private System.Windows.Forms.Label LBLTOTAL;
+        private System.Windows.Forms.Label lblpreco4;
+        private System.Windows.Forms.Label LBLpreco2;
+        private System.Windows.Forms.Label lblpreco3;
+        private System.Windows.Forms.Label LBLpreco1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LBLtotalvalor;
+        private System.Windows.Forms.TextBox txtdinheiro;
+    }
+}
+
---
+++ aula05MaquinaDeProdutos/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula05MaquinaDeProdutos
+{
+    public partial class BTNcompra : Form
+    {
+        public BTNcompra()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTNmarmita2_Click(object sender, EventArgs e)
+        {
+            double marmita2 = 18;
+            double total = int.Parse(LBLtotalvalor.Text);
+            double soma = marmita2 + total;
+            LBLtotalvalor.Text = soma.ToString();
+        }
+
+        private void LBLvalor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTNcomprar_Click(object sender, EventArgs e)
+        {
+            double total = int.Parse(LBLtotalvalor.Text );
+
+            int pagar;
+            pagar = int.Parse(txtdinheiro.Text);
+            if(pagar >= total)
+            {
+                MessageBox.Show("COMPRA FINALIZADA");
+            }
+            else
+            {
+                MessageBox.Show("DINHEIRO INSUFICIENTE!!");
+            }
+            
+            
+
+
+        }
+
+        private void BTNmarmita1_Click(object sender, EventArgs e)
+        {
+            double marmita1 = 16;
+            double total = int.Parse(LBLtotalvalor.Text);
+            double soma = marmita1 + total;
+            LBLtotalvalor.Text = soma.ToString();
+            
+        }
+
+        private void BTNmarmita3_Click(object sender, EventArgs e)
+        {
+            double marmita3 = 15;
+            double total = int.Parse(LBLtotalvalor.Text);
+            double soma = marmita3 + total;
+            LBLtotalvalor.Text = soma.ToString();
+        }
+
+        private void BTNmarmita4_Click(object sender, EventArgs e)
+        {
+            double marmita4 = 20;
+            double total = int.Parse(LBLtotalvalor.Text);
+            double soma = marmita4 + total;
+            LBLtotalvalor.Text = soma.ToString();
+        }
+
+        private void BTNCancelar_Click(object sender, EventArgs e)
+        {
+            
+            LBLtotalvalor.Text = Convert.ToString("0");
+        }
+
+        private void txtdinheiro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LBLtotalvalor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
